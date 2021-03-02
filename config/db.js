ed822a47f7f1e6c7aa9addb9a0e44700d83af59d
kassidyb816 <kassidy.barnes65@gmail.com>
2021-03-02 13:43:16 -0600

    config and package.json

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config'); //use config file
+const db = config.get('mongoURI'); //data in the default.json file for application wide use
+
+const connectDB = async () => {
+    try {
+      await mongoose.connect(db, {
+          useNewUrlParser: true
+      });
+      
+      console.log('MongoDB Connected...');
+    } catch(err) {
+      console.log(err.message);
+      // exit the process with failure 
+      process.exit(1); 
+
+    }
+}
+module.exports = connectDB; //method to connect database
