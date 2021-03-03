e629248c20a595e2d7048c53f4e0bbc5bf5173c2
kassidyb816 <kassidy.barnes65@gmail.com>
2021-03-03 17:51:18 -0600

    register users
    email, encrypted password, gravatar

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get('mongoURI'); //data in the default.json file for applicati
 const connectDB = async () => {
     try {
       await mongoose.connect(db, {
-          useNewUrlParser: true
+          useNewUrlParser: true,
+          useCreateIndex: true
       });
       
       console.log('MongoDB Connected...');
