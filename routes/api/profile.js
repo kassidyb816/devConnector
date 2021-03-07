042cfe3997cda6394c52b3507ed82decbb47ea66
kassidyb816 <kassidy.barnes65@gmail.com>
2021-03-06 19:54:59 -0600

    Profile Schema, models, and endpoints

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route   GET api/profile
-// @des     Test route
-// @access  Public
-router.get('/', (req,res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @des     Get current users profile
+// @access  private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id}).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ mes: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
